f44cc00de515a2b807b9ffaf4473b431cd32f81b
nimonian <nimon@hotmail.co.uk>
2023-12-08 11:40:22 +0000

    concision

--- 08/b.py
+++ 08/b.py
@@ -9,26 +9,18 @@ then = time()
 path = (Path(__file__).parent / "./input.txt").resolve()
 lines = path.read_text().split("\n\n")
 
-# get directions
 rl = lines[0]
 rl = [0 if dir == "L" else 1 for dir in rl]
 
-# get nodes
 nodes = lines[1].split("\n")
 nodes = [re.findall(r"([A-Z]+)", node) for node in nodes]
 nodes = {node[0]: (node[1], node[2]) for node in nodes}
 
+lcm = lambda x, y: x * y // gcd(x, y)
+lcm_list = lambda nums: reduce(lcm, nums)
+
 locs = [loc for loc in nodes.keys() if loc[-1] == "A"]
 results = []
-
-
-# least common multiple
-def lcm(x, y):
-    return x * y // gcd(x, y)
-
-
-def lcm_list(nums):
-    return reduce(lcm, nums)
 
 
 for loc in locs:
